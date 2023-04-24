8effba7eadbd34392a2edb9376780c69136738f7
magnus <magnus.wahlstrand@gmail.com>
2023-04-24 08:00:25 +0200

    get clerk users

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,16 +1,23 @@
-import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import type { User } from "@clerk/nextjs/api";
+import { clerkClient } from "@clerk/nextjs/server";
+
+const filterUserForClient = (user: User) => {
+  return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl };
+};
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // TODO: Why do I get this error?
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    
+    const users = (await clerkClient.users.getUserList({
+      limit: 100
+    })).map(filterUserForClient);
+
+    return posts.map((post) => ({
+        author: users.find((user) => user.id === post.authorId)
+      }
+    ));
+  })
 });
